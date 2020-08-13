828f685128cc2fa4b845286ba3c4bdc300c83fed
IllIdanIllI <ilya_galica@mail.ru>
2020-08-13 17:59:21 +0300

    Add two entities

---
+++ src/main/java/test/task/roomBooking/repositity/model/Room.java
@@ -0,0 +1,27 @@
+package test.task.roomBooking.repositity.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Builder
+@Data
+@Entity
+@Table(name = "room")
+public class Room implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "roomIdSequencer")
+    @SequenceGenerator(name = "roomIdSequencer", sequenceName = "room_id_seq", allocationSize = 1)
+    private Integer id;
+
+    @Column
+    private int number;
+    
+    @OneToOne
+    @JoinColumn(name = "room_type_id", referencedColumnName = "id")
+    private RoomType type;
+}
---
+++ src/main/java/test/task/roomBooking/repositity/model/RoomType.java
@@ -0,0 +1,23 @@
+package test.task.roomBooking.repositity.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Builder
+@Data
+@Entity
+@Table(name = "room_type")
+public class RoomType implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "room_typeIdSequencer")
+    @SequenceGenerator(name = "room_typeIdSequencer", sequenceName = "room_type_id_seq", allocationSize = 1)
+    private Integer id;
+
+    @Column
+    private String name;
+}
